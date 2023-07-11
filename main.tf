09aaf55002cbe59fb0d46a84104afa229a380b27
Sibabalwe Ngqoyiya <sngqoyiya@plusonex.com>
2023-07-11 14:38:20 +0200

    testing

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@
 resource "aws_vpc" "random" {
   name                 = "vpc-test"
   enable_dns_hostnames = true
-  enable_dns_support   = strue
+  enable_dns_support   = true
   cidr_block           = "10.0.0.0/16"
 }
